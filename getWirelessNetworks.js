d9deacf284ab507e43749bd6609dc1b6f5cc57cc
Volumio <info@volumio.org>
2024-07-19 13:28:38 +0200

    Helper scripts

---
+++ getWirelessNetworks.js
@@ -0,0 +1,16 @@
+const path = require('path');
+const dotenv = require('dotenv').config({ path: path.join(__dirname, '.env')}); // eslint-disable-line
+const io = require('socket.io-client');
+
+
+const websocketHost = process.env.WEBSOCKET_HOST;
+const socket = io.connect('http://' + websocketHost + ':3000');
+
+socket.on('connect', function() {
+    console.log(' Client Connected');
+    socket.emit('getWirelessNetworks');
+    socket.on('pushWirelessNetworks', function(data) {
+        console.log('%j', data);
+    })
+});
+
